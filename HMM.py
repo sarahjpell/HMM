c757251d9789e75c6150b7bf923b2f429eeb30cf
Sarah Pell <spell1@uvm.edu>
2019-04-16 19:31:46 -0400

    ok.. better....

--- HMM.py
+++ HMM.py
@@ -65,18 +65,18 @@ for r in range(2, len(states)):
     vMatrix[1][r] = 0
     # print vMatrix[1][r]
 
-hi_opt = []
-lo_opt = []
+# hi_opt = []
+# lo_opt = []
 mult_opt = "NO"
 
 for letter in range(2, len(sequence)):
     for state in range(2, len(states)):
         if letter == 2:
             vMatrix[letter][state] = start * log_emission[states[state]][sequence[letter]]
-            if state == 2:
-                hi_opt.append('H')
-            else:
-                lo_opt.append('L')
+            # if state == 2:
+            #     hi_opt.append('H')
+            # else:
+            #     lo_opt.append('L')
         else:
             # if state = 2 : in hi state -- check hi-hi, hi-lo
             if state == 2:
@@ -87,14 +87,14 @@ for letter in range(2, len(sequence)):
 
                 if pos_pos > pos_neg:
                     max_val = pos_pos
-                    hi_opt.append('H')
+                    # hi_opt.append('H')
                 elif pos_neg > pos_pos:
                     max_val = pos_neg
-                    hi_opt.append('L')
-                else:
-                    max_val = pos_pos
-                    hi_opt.append('=')
-                    mult_opt = 'YES'
+                    # hi_opt.append('L')
+                # else:
+                #     max_val = pos_pos
+                #     hi_opt.append('=')
+                #     mult_opt = 'YES'
                 vMatrix[letter][state] = max_val
 
             # if state = 3 : in low state -- check lo-hi, lo-lo
@@ -105,20 +105,32 @@ for letter in range(2, len(sequence)):
                           log_transition[states[state - 1]][states[state - 1]]
                 if neg_neg > neg_pos:
                     max_val = neg_neg
-                    lo_opt.append('L')
+                    # lo_opt.append('L')
                 elif neg_pos > neg_neg:
                     max_val = neg_pos
-                    lo_opt.append('H')
-                else:
-                    max_val = neg_pos
-                    lo_opt += '='
-                    mult_opt = 'YES'
+                    # lo_opt.append('H')
+                # else:
+                #     max_val = neg_pos
+                #     lo_opt += '='
+                #     mult_opt = 'YES'
                 vMatrix[letter][state] = max_val
 
             else:
                 print "you messed up indexing"
 
 
+#find optimum path
+opt_path = []
+for c in range(2, len(sequence)):
+    hi_val = vMatrix[c][2]
+    lo_val = vMatrix[c][3]
+    if hi_val > lo_val:
+        opt_path.append('H')
+    elif lo_val > hi_val:
+        opt_path.append('L')
+    else:
+        opt_path.append('=')
+        mult_opt = 'YES'
 
 print "matrix"
 for item in range(len(states)):
@@ -128,52 +140,9 @@ for item in range(len(states)):
         else:
             print vMatrix[another][item],
 
-for something in hi_opt:
-    print something,
-print
 print mult_opt
-print
-for e in lo_opt:
-    print e,
-# # initialize table
-# vtable = [{}]
-#
-# for s in states:
-#     vtable[0][s] = {"probability": start * log_emission[s][sequence[0]], "previous": None}
-#
-# for t in range(1, len(sequence)):
-#     vtable.append({})
-#     for st in states:
-#         max_tr_prob = vtable[t - 1][states[0]]["probability"] * log_transition[states[0]][st]
-#         prev_prob_sel = states[0]
-#         for prev_st in states[1:]:
-#             tr_prob = vtable[t - 1][prev_st]["probability"] * log_transition[prev_st][st]
-#             if tr_prob > max_tr_prob:
-#                 max_tr_prob = tr_prob
-#                 prev_prob_sel = prev_st
-#         max_prob = max_tr_prob * log_emission[st][sequence[t]]
-#         vtable[t][st] = {"probability": max_prob, "previous": prev_prob_sel}
-# # for i in range(len(vtable)):
-# #     print " ".join(sequence[i])
-# #     for state in vtable[0]:
-# #         for v in vtable:
-# #             print state, " ".join(v[state]["probability"])
-# optimal = []
-# max_prob = max(value["probability"] for value in vtable[-1].values())
-# previous = None
-# for st, data in vtable[-1].items():
-#     if data["probability"] == max_prob:
-#         optimal.append(st)
-#         previous = st
-#         break
-#
-# for t in range(len(vtable)-2, -1, -1):
-#     optimal.insert(0, vtable[t+1][previous]["previous"])
-#     previous = vtable[t+1][previous]["previous"]
-#
-# print optimal
-# print max_prob
-
+for thing in opt_path:
+    print thing,
 # A: P(x) given model
 
 # B: write complete viterbi table
