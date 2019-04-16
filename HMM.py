1cb5fb2b425b2448b6d3793982f963dbc00804e3
Sarah Pell <spell1@uvm.edu>
2019-04-16 19:23:32 -0400

    mostly working, probably a kink somewhere making values off slightly... hm
    need to do forward/backward next

--- HMM.py
+++ HMM.py
@@ -42,24 +42,22 @@ log_transition = {
     'Hi': {'Hi': math.log(0.5, 2), 'Lo': math.log(0.5, 2)},
     'Lo': {'Hi': math.log(0.4, 2), 'Lo': math.log(0.6, 2)}
 }
+
 sequence = "-0" + sequence
-
 vMatrix = [[0 for x in range(len(states) + 1)] for y in range(len(sequence) + 1)]
-
 vMatrix[0][0] = '-'
+
 # initialize i,0
 for i in range(1, len(sequence)):
     vMatrix[i][0] = sequence[i]
-    # print vMatrix[i][0]
 
 # intialize 0,j
 for j in range(1, len(states)):
     vMatrix[0][j] = states[j]
-    # print vMatrix[0][j]
 
 vMatrix[1][1] = 1
 # initialize second row/col
-for c in range(2, len(sequence)):
+for c in range(2, len(sequence) - 1):
     vMatrix[c][1] = 0
     # print vMatrix[c][2]
 # print "next"
@@ -67,33 +65,76 @@ for r in range(2, len(states)):
     vMatrix[1][r] = 0
     # print vMatrix[1][r]
 
-opt_states = []
+hi_opt = []
+lo_opt = []
 mult_opt = "NO"
-ct = len(sequence) - 1
 
-while ct != 0:
-    for letter in range(2, len(sequence)):
-        for state in range(2, len(states)):
-            # multiply by START
-            if letter == 2:
-                vMatrix[letter][state] = start * log_emission[states[state]][sequence[letter]]
-                # TODO: THIS STUFF
+for letter in range(2, len(sequence)):
+    for state in range(2, len(states)):
+        if letter == 2:
+            vMatrix[letter][state] = start * log_emission[states[state]][sequence[letter]]
+            if state == 2:
+                hi_opt.append('H')
             else:
-                pass
+                lo_opt.append('L')
+        else:
             # if state = 2 : in hi state -- check hi-hi, hi-lo
+            if state == 2:
+                pos_pos = vMatrix[letter - 1][state] * log_emission[states[state]][sequence[letter]] * \
+                          log_transition[states[state]][states[state]]
+                pos_neg = vMatrix[letter - 1][state + 1] * log_emission[states[state]][sequence[letter]] * \
+                          log_transition[states[state + 1]][states[state + 1]]
+
+                if pos_pos > pos_neg:
+                    max_val = pos_pos
+                    hi_opt.append('H')
+                elif pos_neg > pos_pos:
+                    max_val = pos_neg
+                    hi_opt.append('L')
+                else:
+                    max_val = pos_pos
+                    hi_opt.append('=')
+                    mult_opt = 'YES'
+                vMatrix[letter][state] = max_val
 
             # if state = 3 : in low state -- check lo-hi, lo-lo
+            elif state == 3:
+                neg_neg = vMatrix[letter - 1][state] * log_emission[states[state]][sequence[letter]] * \
+                          log_transition[states[state]][states[state]]
+                neg_pos = vMatrix[letter - 1][state - 1] * log_emission[states[state]][sequence[letter]] * \
+                          log_transition[states[state - 1]][states[state - 1]]
+                if neg_neg > neg_pos:
+                    max_val = neg_neg
+                    lo_opt.append('L')
+                elif neg_pos > neg_neg:
+                    max_val = neg_pos
+                    lo_opt.append('H')
+                else:
+                    max_val = neg_pos
+                    lo_opt += '='
+                    mult_opt = 'YES'
+                vMatrix[letter][state] = max_val
+
+            else:
+                print "you messed up indexing"
 
 
 
+print "matrix"
+for item in range(len(states)):
+    for another in range(len(vMatrix) - 1):
+        if another == len(vMatrix) - 2:
+            print vMatrix[another][item]
+        else:
+            print vMatrix[another][item],
 
-
-
-
-
-
-
-
+for something in hi_opt:
+    print something,
+print
+print mult_opt
+print
+for e in lo_opt:
+    print e,
 # # initialize table
 # vtable = [{}]
 #
