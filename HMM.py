b917b6dd9f8833e95f55a26d18c8fce41635c10c
Sarah Pell <spell1@uvm.edu>
2019-04-15 21:44:39 -0400

    finished viterbi table... TODO: print out matrix, get probability of optimal

--- HMM.py
+++ HMM.py
@@ -7,10 +7,6 @@ import math
 fileName = raw_input("What is the name of your input file: ")
 f = open(fileName, 'r')
 
-# seqNames = []
-# sequence = []
-# nameCt = 1
-# seqCt = 0
 sequence = ""
 # put sequence name and sequences into arrays
 for line in f:
@@ -18,11 +14,57 @@ for line in f:
 f.close()
 print sequence
 
-# A: P(x) given model
-# hmm:
+# sequence = "0" + s
+
+# model for forward/backward:
+states = ("Hi", "Lo")
 start = 0.5
-emission = {'H': {'A': 0.2, 'C': 0.3, 'G': 0.3, 'T': 0.2}, 'L': {'A': 0.3, 'C': 0.2, 'G': 0.2, 'T': 0.3}}
-transition = {'H': {'H': 0.5, 'L': 0.5}, 'L': {'H': 0.4, 'L': 0.6}}
+emission = {
+    'Hi': {'A': 0.2, 'C': 0.3, 'G': 0.3, 'T': 0.2},
+    'Lo': {'A': 0.3, 'C': 0.2, 'G': 0.2, 'T': 0.3}
+}
+transition = {
+    'Hi': {'Hi': 0.5, 'Lo': 0.5},
+    'Lo': {'Hi': 0.4, 'Lo': 0.6}
+}
+
+# log model for viterbi
+log_emission = {
+    'Hi': {'A': math.log(0.2, 2), 'C': math.log(0.3, 2), 'G': math.log(0.3, 2), 'T': math.log(0.2, 2)},
+    'Lo': {'A': math.log(0.3, 2), 'C': math.log(0.2, 2), 'G': math.log(0.2, 2), 'T': math.log(0.3, 2)}
+}
+log_transition = {
+    'Hi': {'Hi': math.log(0.5, 2), 'Lo': math.log(0.5, 2)},
+    'Lo': {'Hi': math.log(0.4, 2), 'Lo': math.log(0.6, 2)}
+}
+
+# initialize table
+vtable = [{}]
+
+for s in states:
+    vtable[0][s] = {"probability": start * log_emission[s][sequence[0]], "previous": None}
+
+for t in range(1, len(sequence)):
+    vtable.append({})
+    for st in states:
+        max_tr_prob = vtable[t - 1][states[0]]["probability"] * log_transition[states[0]][st]
+        prev_prob_sel = states[0]
+        for prev_st in states[1:]:
+            tr_prob = vtable[t - 1][prev_st]["probability"] * log_transition[prev_st][st]
+            if tr_prob > max_tr_prob:
+                max_tr_prob = tr_prob
+                prev_prob_sel = prev_st
+        max_prob = max_tr_prob * log_emission[st][sequence[t]]
+        vtable[t][st] = {"probability": max_prob, "previous": prev_prob_sel}
+for i in range(len(vtable)):
+    print " ".join(sequence[i])
+    for state in vtable[0]:
+        for v in vtable:
+            print state, " ".join(v[state]["probability"])
+optimal = []
+
+
+# A: P(x) given model
 
 # B: write complete viterbi table
 
